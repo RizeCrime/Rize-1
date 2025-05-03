eb59372d1cf62db68a637ba8879954e456d02ad6
heydocode <169077757+heydocode@users.noreply.github.com>
2025-05-03 13:39:04 +0200

    Implement fetch, decode, execute bevy systems

--- src/interpreter/systems.rs
+++ src/interpreter/systems.rs
@@ -3,8 +3,9 @@
 use bevy::prelude::*;
 
 use crate::{
-    types::{ActiveProgram, Registers},
+    types::{ActiveProgram, Registers, SystemMemory}, 
     CpuCycleStage,
+    display::DisplayMemory,
 };
 
 use super::InterpreterRes;
@@ -16,18 +17,56 @@ pub fn fetch(
     interpreters: Res<InterpreterRes>,
 ) {
     let interpreter = interpreters.active.as_ref().unwrap();
-    // if None, assume EOF and Halt
     if interpreter
         .fetch(&mut registers, &mut r_active_program)
         .is_none()
     {
+        // Fetching errors won't let Decoding stage reading the actual line to 
+        //   decode (which results in ghosting CpuCycle)
         sn_cpu.set(CpuCycleStage::Halt);
     }
 }
 
 pub fn decode(
-    mut r_active_program: ResMut<ActiveProgram>,
+    mut memory: ResMut<SystemMemory>,
+    mut registers: ResMut<Registers>,
+    mut program: ResMut<ActiveProgram>,
+    mut sn_cpu: ResMut<NextState<CpuCycleStage>>,
     interpreters: Res<InterpreterRes>,
 ) {
     let interpreter = interpreters.active.as_ref().unwrap();
+    if let Err(e) = interpreter.decode(&mut program, &mut registers, &mut memory) {
+        error!("Decoding Error: {:?}", e.type_,);
+        // Decoding errors will compromise the Execute stage.
+        //   For example, by not validating the arguments,
+        //   Execute stage gets unexpected behavior (e.g. ignored instructions)
+        sn_cpu.set(CpuCycleStage::Halt);
+    }
+}
+
+pub fn execute(
+    interpreters: Res<InterpreterRes>,
+    mut registers: ResMut<Registers>,
+    mut program: ResMut<ActiveProgram>,
+    mut memory: ResMut<SystemMemory>,
+    mut display_memory: ResMut<DisplayMemory>,
+    images: Res<Assets<Image>>,
+    mut sn_cpu: ResMut<NextState<CpuCycleStage>>,
+    mut next_cpu_stage: ResMut<NextState<CpuCycleStage>>,
+) {
+    let interpreter = interpreters.active.as_ref().unwrap();
+    if interpreter
+        .execute(
+            &mut program,
+            &mut registers,
+            &mut memory,
+            &mut display_memory,
+            &images,
+        )
+        .is_none()
+    {
+        // Halting prevents from ignoring instructions, that are incorrect/causing unexpected behavior
+        // Clearly indicating that an error has occured is considerably better than unexpected behavior.
+        sn_cpu.set(CpuCycleStage::Halt);
+    }
 }
